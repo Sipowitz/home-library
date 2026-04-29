e7942128524001f4ba6bb805f88b229ec7744cf2
root <root@localhost.localdomain>
2026-04-29 16:23:29 +0100

    dev: working HTTPS setup with mkcert + Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync("./192.168.1.200+1-key.pem"),
+      cert: fs.readFileSync("./192.168.1.200+1.pem"),
+    },
+    host: true,
+  },
+});
