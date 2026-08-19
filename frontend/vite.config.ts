f449dcf1d71f5985ed812d5fb1bc8185dce948e0
Graeme McKenzie <g_w_mckenzie@yahoo.co.uk>
2026-08-19 19:08:01 +0100

    Update local development HTTPS and Docker configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,21 +6,22 @@ export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync("./192.168.1.200-key.pem"),
-      cert: fs.readFileSync("./192.168.1.200.pem"),
+      key: fs.readFileSync("./192.168.10.101-key.pem"),
+      cert: fs.readFileSync("./192.168.10.101.pem"),
     },
+
     host: true,
 
     proxy: {
       "/api": {
-        target: "http://192.168.1.200:8000",
+        target: "http://192.168.10.101:8001",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
 
       "/covers": {
-        target: "http://192.168.1.200:8000",
+        target: "http://192.168.10.101:8001",
         changeOrigin: true,
         secure: false,
       },
